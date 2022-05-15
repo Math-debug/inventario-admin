59cb0bad47b40463c8a654512f8987411b60f57e
Math-debug <matheus.araujo.xavier@gmail.com>
2022-05-15 15:33:30 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<Login/>
+</template>
+
+<script>
+import Login from './views/Login';
+
+export default {
+  name: 'App',
+
+  components: {
+    Login,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
